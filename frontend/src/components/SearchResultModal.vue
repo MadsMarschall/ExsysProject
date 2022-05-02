345c1937131f1fed64971390754cabaa5d4578fa
Mads Marschall <marschall.mads@live.dk>
2022-05-02 02:21:45 +0200

    layout adjustments and refactoring

--- frontend/src/components/SearchResultModal.vue
+++ frontend/src/components/SearchResultModal.vue
@@ -1,48 +1,38 @@
 <template>
-  <b-container
-    fluid
-    v-show="isSearching"
-    class="position-absolute pt-0 mt-5"
-    style="z-index: 20"
-    ref="searchResultContainer"
-  >
-    <b-row align-h="center" class="w-100">
-      <b-col lg="4" md="6" cols="12">
-        <b-list-group>
-          <b-list-group-item
-            v-for="dp in dataProducts"
-            v-bind:key="dp.id"
+  <b-list-group>
+    <b-list-group-item
+      v-for="dp in dataProducts"
+      v-bind:key="dp.id"
+      href="#"
+      style="z-index: 100"
+      :to="'/dataproducts/' + dp.id"
+    >
+      <div class="d-flex justify-content-between">
+        <h5 class="pt-2 pb-2">{{ dp.name }}</h5>
+        <small class="text-muted">3 days ago</small>
+      </div>
+      <b-row>
+        <b-col align="left">
+          <p class="font-italic float-left">Tags:</p>
+          <b-button
+            v-for="(tag, index) in dp.tags"
+            v-bind:key="index"
+            size="sm"
             href="#"
-            class="flex-column align-items-start"
-          >
-            <div class="d-flex w-100 justify-content-between">
-              <h5 class="pt-2 pb-2">{{ dp.name }}</h5>
-              <small class="text-muted">3 days ago</small>
-            </div>
-            <b-row class="w-100">
-              <b-col align="left">
-                <p class="font-italic float-left">Tags:</p>
-                <b-button
-                  v-for="(tag, index) in dp.tags"
-                  v-bind:key="index"
-                  size="sm"
-                  href="#"
-                  class="m-1 float-left"
-                  style="max-height: 30px"
-                  ><p class="text-light font-italic">{{ tag }}</p>
-                </b-button>
-              </b-col>
-            </b-row>
-          </b-list-group-item>
-        </b-list-group>
-      </b-col>
-    </b-row>
-  </b-container>
+            class="m-1 float-left"
+            style="max-height: 30px"
+            ><p class="text-light font-italic">{{ tag }}</p>
+          </b-button>
+        </b-col>
+      </b-row>
+    </b-list-group-item>
+  </b-list-group>
 </template>
 
 <script lang="ts">
 import Vue, { PropType } from "vue";
 import { IReport } from "@/interfaces";
+import { mixin as clickaway } from "vue-clickaway";
 
 export default Vue.extend({
   props: {
@@ -58,6 +48,7 @@ export default Vue.extend({
   data: function () {
     return {};
   },
+  mixins: [clickaway],
   watch: {
     dataProducts: {
       handler: function (newVal: IReport[]) {
--- frontend/src/views/ReportSearchView.vue
+++ frontend/src/views/ReportSearchView.vue
@@ -2,77 +2,39 @@
 
 <template>
   <div class="reportSearch">
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-navbar-nav style="z-index: 10">
-            <b-nav-item href="#">
-              <b-icon icon="bar-chart-fill"></b-icon>
-              Reports
-            </b-nav-item>
-            <b-nav-item href="#">
-              <b-icon icon="folder"></b-icon>
-              Reports
-            </b-nav-item>
-            <b-nav-item href="#">
-              <b-icon icon="layers-fill"></b-icon>
-              Apps
-            </b-nav-item>
-          </b-navbar-nav>
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="position-absolute w-100">
-          <b-nav-form class="ml-auto mr-auto">
-            <b-input-group>
-              <b-form-input
-                size="sm"
-                class="mr-sm-2"
-                placeholder="Search"
-                v-model="searchTerm"
-              ></b-form-input>
-            </b-input-group>
-          </b-nav-form>
-          <SearchResultModal
-            :data-products="dataProductSearch"
-            :is-searching="isSearching"
-          />
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+    <BIGNavBar />
     <b-container fluid>
       <b-row class="pt-5">
         <b-col
           v-for="rep in dataProducts"
-          lg="2"
-          md="4"
+          lg="3"
+          md="6"
           sm="12"
           v-bind:key="rep.id"
         >
-          <b-card
-            img-src="https://picsum.photos/600/300/?image=25"
-            img-alt="Image"
-            img-top
-            img-height="180"
-            tag="article"
-            class="mb-2"
-            :title="rep.name"
-          >
-            <template #header>
-              <b-col>
-                <b-icon
-                  icon="gear"
-                  style="right: 0px; position: absolute; top: -8px"
-                  v-b-modal.my-modal
-                ></b-icon>
-              </b-col>
-            </template>
+          <router-link :to="'/dataproducts/' + rep.id">
+            <b-card
+              img-src="https://picsum.photos/600/300/?image=25"
+              img-alt="Image"
+              img-top
+              img-height="180"
+              tag="article"
+              class="mb-4"
+              :title="rep.name"
+            >
+              <template #header>
+                <b-col>
+                  <b-icon
+                    icon="gear"
+                    style="right: 0px; position: absolute; top: -8px"
+                    v-b-modal.my-modal
+                  ></b-icon>
+                </b-col>
+              </template>
 
-            <b-card-text></b-card-text>
-
-            <b-button v-b-modal.my-modal>Add search tag</b-button>
-          </b-card>
+              <b-card-text></b-card-text>
+            </b-card>
+          </router-link>
         </b-col>
       </b-row>
       <SettingsDialog />
@@ -86,31 +48,17 @@ import { api } from "@/API";
 import { IReport } from "@/interfaces";
 import Vue from "vue";
 import SearchResultModal from "@/components/SearchResultModal.vue";
+import BIGNavBar from "@/components/BIGNavBar.vue";
 
 interface IData {
   dataProducts: IReport[];
 }
 
 export default Vue.extend({
-  components: { SearchResultModal, SettingsDialog },
+  components: { BIGNavBar, SettingsDialog },
   data: () => {
     let dataProducts = new Array<IReport>();
-    let searchTerm = "";
-    let dataProductSearch = new Array<IReport>();
-    let isSearching = false;
-    return { dataProducts, searchTerm, dataProductSearch, isSearching };
-  },
-  watch: {
-    searchTerm: {
-      handler: async function (newVal, oldVal) {
-        if (newVal.length < 1) {
-          this.isSearching = false;
-          return;
-        }
-        this.isSearching = true;
-        this.dataProductSearch = await api.getDataProductByTagSearch(newVal);
-      },
-    },
+    return { dataProducts };
   },
   async mounted(): Promise<void> {
     this.dataProducts = await api.getAllDataProducts();
