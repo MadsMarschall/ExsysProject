d7042644ec5e75c38824d122f2ea802d3343f215
Mads Marschall <marschall.mads@live.dk>
2022-05-17 08:19:50 +0200

    Creating search strategy pattern

--- backend/src/interfaces/ISearchStrategy.ts
+++ backend/src/interfaces/ISearchStrategy.ts
@@ -1,5 +1,8 @@
 import IReport from "./IDataProduct";
+import IDataProduct from "./IDataProduct";
 
 export interface ISearchStrategy {
   search(term:string):IReport[];
+
+  indexContent(newReports: IDataProduct[]):void;
 }
